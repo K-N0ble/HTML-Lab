8bdcc6408f4b5630a24b300379a5fafb89f7b223
K-N0ble <kristen.noble1@edu.sait.ca>
2024-11-15 14:42:30 -0700

    feature: year displayed in footer now reads current year and displays it

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//sanity check
+console.log("Hello World!");
+
+//footer copyright date
+const fYear = document.getElementById("footerYear");
+const d = new Date();
+fYear.textContent = d.getFullYear();
+
+console.log(`Today's date is: ${d}`);
+
+//greeting
+
+
+
