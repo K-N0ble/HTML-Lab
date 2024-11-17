28a1f03ad7fe323862790140ec2ae936ef64242d
K-N0ble <kristen.noble1@edu.sait.ca>
2024-11-17 15:36:50 -0700

    feat: add adaptive style changes to greeting message

--- js/main.js
+++ js/main.js
@@ -9,13 +9,29 @@ fYear.textContent = d.getFullYear();
 console.log(`Today's date is: ${d}`);
 
 //greeting
-const greet = document.getElementById("greeting");
+ const greet = document.getElementById("greeting");
+ const greetingMessage = document.getElementById("message");
+ const morning = document.getElementById("morning");
+ const afternoon = document.getElementById("afternoon");
+ const evening = document.getElementById("evening");
+
 if (d.getHours() <= 11) {
     greet.textContent = "Good morning!";
+    greetingMessage.style.background = "rgb(255, 255, 204";
+    greet.style.color = "black";
+    afternoon.style.display = "none";
+    evening.style.display = "none";
 } else if (d.getHours() <= 18){ 
     greet.textContent = "Good afternoon!";
+    greetingMessage.style.background = "rgb(255, 167, 133)";
+    morning.style.display = "none";
+    evening.style.display = "none";
 } else {
     greet.textContent = "Good evening!";
+    greetingMessage.style.background = "rgb(176, 180, 207)";
+    morning.style.display = "none";
+    afternoon.style.display = "none";
 }
+
 
 
